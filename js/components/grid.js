f8b37b899fd037ef7dcefc73a811e9a2e7cbfc1b
Vadim Sokolovski <sweetfrontend@gmail.com>
2017-02-28 23:04:23 +0300

    Create makeVerticalScrollable public method:
    Implement custom vertical scroll

--- js/components/grid.js
+++ js/components/grid.js
@@ -1,4 +1,73 @@
 (function(externalParent) {
+    var GridHelper = {
+        moveThumb: function(position, scrollThumb, scrollHeight) {
+            var thumbHeight;
+            thumbHeight = scrollThumb.offsetHeight;
+
+            if (position <= 0) {
+                scrollThumb.style.top = 0;
+
+            } else if(position + thumbHeight >= scrollHeight) {
+                scrollThumb.style.top = scrollHeight - thumbHeight + 'px';
+
+            } else {
+                scrollThumb.style.top = position + 'px';
+            }
+        }
+    };
+    var EventHelper = {
+        preventDefaultBehavior: function(event) {
+            event.preventDefault();
+        },
+        handleFocusY: function(scrollY, scrollHeight, scrollThumb) {
+            var keyDownHandler = function(event) {
+                event.preventDefault();
+
+                var position = scrollThumb.offsetTop;
+                var key = event.which;
+
+                if (key === 38) {
+                    GridHelper.moveThumb(--position, scrollThumb, scrollHeight);
+
+                } else if (key === 40) {
+                    GridHelper.moveThumb(++position, scrollThumb, scrollHeight);
+                }
+            };
+            scrollThumb.addEventListener('keydown', keyDownHandler);
+            scrollThumb.addEventListener('blur', function blurHandler() {
+                this.removeEventListener('blur', blurHandler);
+                this.removeEventListener('keydown', keyDownHandler);
+            });
+        },
+        handleMouseDownY: function (scrollY, scrollHeight, scrollThumb, event) {
+            event.preventDefault();
+
+            if (event.target === scrollThumb) {
+                var gridElement = this._gridElement;
+                var scrollThumbY = scrollThumb.getBoundingClientRect().top + pageYOffset;
+                var scrollThumbOffset = event.pageY - scrollThumbY;
+                scrollThumb.style.top = event.pageY - scrollY - scrollThumbOffset + 'px';
+
+                var mouseMoveHandler = function(event) {
+                    var position = event.pageY - scrollY - scrollThumbOffset;
+                    GridHelper.moveThumb(position, scrollThumb, scrollHeight);
+                };
+
+                gridElement.addEventListener('mousemove', mouseMoveHandler);
+                document.addEventListener('mouseup', function mouseUpHandler() {
+                    this.removeEventListener('mouseup', mouseUpHandler);
+                    gridElement.removeEventListener('mousemove', mouseMoveHandler);
+                });
+
+               scrollThumb.focus();
+
+            } else if (event.target === scrollThumb.parentNode) {
+                var position = event.pageY - scrollY - scrollThumb.offsetHeight/2;
+                GridHelper.moveThumb(position, scrollThumb, scrollHeight);
+            }
+        }
+    };
+
     function Grid(data) {
         if (!(this instanceof Grid)) {
             return new Grid(data);
@@ -9,7 +78,22 @@
     }
 
     var Prototype = {
-        constructor: Grid
+        constructor: Grid,
+        makeVerticalScrollable: function() {
+            var gridElement = this._gridElement;
+            var vScroll = gridElement.querySelector('.vertical-scroll');
+            var vScrollY = vScroll.getBoundingClientRect().top + pageYOffset;
+            var vScrollHeight = vScroll.offsetHeight;
+            var vScrollThumb = vScroll.querySelector('.scroll-thumb');
+
+            var preventDefaultBehavior = EventHelper.preventDefaultBehavior;
+            var mouseDownHandler = EventHelper.handleMouseDownY;
+            var focusHandler = EventHelper.handleFocusY;
+
+            document.ondragstart = preventDefaultBehavior;
+            vScroll.addEventListener('mousedown', mouseDownHandler.bind(this, vScrollY, vScrollHeight, vScrollThumb));
+            vScrollThumb.addEventListener('focus', focusHandler.bind(null, vScrollY, vScrollHeight, vScrollThumb));
+        }
     };
 
     Grid.prototype = Prototype;

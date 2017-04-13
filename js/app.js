7a2f19b164d7012432fcc58dba174ed48a19c589
Vadim Sokolovski <sweetfrontend@gmail.com>
2017-04-13 22:00:58 +0300

    Updated grid component

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 (function() {
+    // Test data
+    var getData = function() {
+        var data = [];
+        var length = 50;
+
+        for (var i = 0; i < length; i++) {
+            data.push({
+                name: 'Anna Trujillo',
+                contactName: 'Anna Trujillo',
+                contactTitle: 'Owner',
+                companyName: 'Anna Trujillo Emparedados y helados',
+                country: 'Mexico'
+            });
+        }
+
+        return data;
+    };
     var Grid = Yonder.components.Grid;
-    var grid = new Grid();
+    var grid = new Grid(getData());
 }());
--- js/components/grid.js
+++ js/components/grid.js
@@ -1,20 +1,47 @@
 (function(externalParent) {
     var GridHelper = {
+        initialize: function(data) {
+            var itemsCountSelector = document.querySelector('.items-per-page > select');
+            var itemsCount = +itemsCountSelector.options[itemsCountSelector.selectedIndex].value;
+
+            var mainTemplate = document.getElementById('grid-main-template').innerHTML;
+            var partialTemplate = document.getElementById('grid-partial-template').innerHTML;
+
+            var dataContainer = document.querySelector('.tbl-data');
+
+            var viewTemplate = function(itemsCount) {
+                var html = Mustache.render(mainTemplate, {
+                    visible: data.slice(0, itemsCount),
+                    hidden: data.slice(itemsCount, itemsCount * 2)
+                }, {partialTemplate: partialTemplate});
+
+                dataContainer.innerHTML = html;
+                var dataInContainer = dataContainer.querySelector('.tbl-data-in');
+                var hiddenContainer = dataContainer.querySelector('.tbl-data-in.hidden');
+
+                dataContainer.style.height = dataInContainer.offsetHeight + 'px';
+                hiddenContainer.style.top = dataInContainer.offsetTop + dataInContainer.offsetHeight + 'px';
+            };
+            viewTemplate(itemsCount);
+        },
         moveThumb: function(position, scrollThumb, scrollHeight) {
-            var thumbHeight;
-            thumbHeight = scrollThumb.offsetHeight;
+            var thumbHeight = scrollThumb.offsetHeight;
+            var top;
 
             if (position <= 0) {
-                scrollThumb.style.top = 0;
+                top = 0;
 
-            } else if(position + thumbHeight >= scrollHeight) {
-                scrollThumb.style.top = scrollHeight - thumbHeight + 'px';
+            } else if (position + thumbHeight >= scrollHeight) {
+                top = scrollHeight - thumbHeight;
 
             } else {
-                scrollThumb.style.top = position + 'px';
+                top = position;
             }
+
+            scrollThumb.style.top = top + 'px';
         }
     };
+
     var EventHelper = {
         preventDefaultBehavior: function(event) {
             event.preventDefault();
@@ -74,16 +101,17 @@
         }
 
         this._gridElement = document.querySelector('.grid');
+
+        GridHelper.initialize(data);
         this.makeVerticalScrollable();
     }
 
     var Prototype = {
         constructor: Grid,
         makeVerticalScrollable: function() {
-            var gridElement = this._gridElement;
-            gridElement.classList.add('v-scrollable');
+            this._gridElement.classList.add('v-scrollable');
 
-            var vScroll = gridElement.querySelector('.vertical-scroll');
+            var vScroll = this._gridElement.querySelector('.vertical-scroll');
             var vScrollY = vScroll.getBoundingClientRect().top + pageYOffset;
             var vScrollHeight = vScroll.offsetHeight;
             var vScrollThumb = vScroll.querySelector('.scroll-thumb');
@@ -95,6 +123,9 @@
             document.ondragstart = preventDefaultBehavior;
             vScroll.addEventListener('mousedown', mouseDownHandler.bind(this, vScrollY, vScrollHeight, vScrollThumb));
             vScrollThumb.addEventListener('focus', focusHandler.bind(null, vScrollY, vScrollHeight, vScrollThumb));
+        },
+        disableVerticalScroll: function() {
+            this._gridElement.classList.remove('v-scrollable');
         }
     };
 
